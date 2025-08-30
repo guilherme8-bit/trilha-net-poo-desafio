ae2cd4e73f54d4fc132e9b1c473c977dc0b37a5d
Guilherme Lopes <guilherme16092005@gmail.com>
2025-08-30 10:57:11 -0300

    Desafio concluído git statusgit status

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo10", imei: "1111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone: ");
+Smartphone iphone = new Iphone(numero: "789456", modelo: "Modelo12", imei: "2222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instragram");
